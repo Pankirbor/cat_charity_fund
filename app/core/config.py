4e9e151a57206dc559a0ae8458fd14372662422d
Кирилл Панов <lliril_05@list.ru>
2023-10-19 18:32:39 +0300

    Изменения в config.py, правки в переменных окружения.

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import Optional
 
 from pydantic import BaseSettings, EmailStr
@@ -6,11 +8,13 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     """Класс настроек проекта."""
 
-    app_title: str = "QRKot"
-    db_url: str = "sqlite+aiosqlite:///./fastapi.db"
-    secret: str = "SECRET"
-    admin_email: Optional[EmailStr] = "ADMIN_EMAIL"
-    admin_password: Optional[str] = "ADMIN_PASSWORD"
+    app_title: str = os.getenv("APP_TITLE", default="QRKot")
+    db_url: str = os.getenv("DB_URL", default="sqlite+aiosqlite:///./fastapi.db")
+    secret: str = os.getenv("SECRET", default="string")
+    admin_email: Optional[EmailStr] = os.getenv(
+        "ADMIN_EMAIL", default="admin@example.ru"
+    )
+    admin_password: Optional[str] = os.getenv("ADMIN_PASSWORD", default="123password")
 
     class Config:
         env_file = ".env"
